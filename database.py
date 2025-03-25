56fb470ec2d247cb45d70ad0d4b1ee71061a446f
Anii <79158719+lelouch-of-the-code@users.noreply.github.com>
2025-03-26 05:26:41 +0530

    cbkucukgcewkuc

--- database.py
+++ database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")  # pull from env
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
